9ac516fd5137fd5c5f4e13bb12dda68d792e5b8d
DrewMcB <drew.mcbride89@gmail.com>
2021-03-01 21:33:39 -0600

    Added cosmetic changes to root directory to specify start and end dates. Final push.

--- app.py
+++ app.py
@@ -41,8 +41,8 @@ def home():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/&lt;start&gt;<br/>"
+        f"/api/v1.0/&lt;start&gt/&lt;end&gt"
         # Should start and start/end date not appear here?
     )
 
